b761689ed81a4674b7302d731f8b6aba89458745
Sasha Maldonado <engineerajm@gmail.com>
2015-10-16 00:31:43 -0700

    Fixed gpio.c

--- gpio.c
+++ gpio.c
@@ -1,20 +1,18 @@
 //Set the specified pin on the specified port to be in output (1) or input (0) mode.
 void gpio_set_mode(PORT_t* port, uint8_t pin, uint8_t out_or_in){
-  port->DIR =  setBit(port->DIR, pin, out_or_in);
+  if(out_or_in) port->DIRSET = pin;
+  else port->DIRCLR = pin;
 }
 
 //If the specified pin on the specified port is in output mode, set the output of that pin to be on (1) or off (0).
 inline void gpio_set_out(PORT_t* port, uint8_t pin, uint8_t on_or_off){
-  if(getBit(port->DIR, pin))
-    port->OUT = setBit(port->OUT, pin, on_or_off);
+  if(on_or_off) port->OUTSET = pin;
+  else port->OUTCLR = pin;
 }
 
 //If the specified pin on the specified port is in output mode, toggle the pin’s state (if 1 then 0, if 0 then 1).
 inline void gpio_toggle_out(PORT_t* port, uint8_t pin){
-	//port->OUTSET = pin;
-  if(getBit(port->DIR, pin)) {
-    port->OUT = setBit(port->OUT, pin, !getBit(port->OUT, pin));
-  }
+	port->OUTTGL=pin;
 }
 
 //returns value of bit at a specified position in a specified uint8_t
