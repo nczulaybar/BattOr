3f28cf2cd7a16e0cc5cfe4f0a628ad4073889dcc
Nicolo Zulaybar <nczulaybar@gmail.com>
2015-10-15 22:19:31 -0700

    Blinking lights

--- BattOrProject1/BattOrProject1/main.c
+++ BattOrProject1/BattOrProject1/main.c
@@ -35,10 +35,10 @@ int main(void)
 	//led_on(LED_YELLOW_bm);
 	
 	blink_init();
-	blink_set(LED_RED_bm, 2000);
-	blink_set(LED_GREEN_bm, 4000);
-	blink_set(LED_YELLOW_bm, 6000);
-	
+	blink_set(0, 100/2);
+	blink_set(1, 250/2);
+	blink_set(2, 500/2);
+
     while (1) 
     {
 		blink_ms_timer_update();
--- blink.c
+++ blink.c
@@ -2,18 +2,16 @@
 void blink_init(){
 	//Disable blinks
 	timer_init(&TCC0, 0b11);
-	timer_set(&TCC0, 0b001, 2000); //2000 ticks corresponds to 1ms, thanks to the 2MHz peripheral clock
+	timer_set(&TCC0, 0b1, 250); //250 ticks corresponds to 1ms, as of 10/15
 }
 
 ISR(TCC0_OVF_vect){
 	globalTime++;
 }
-//Exactly what we're supposed to do with this is unclear
-//Interrupt handler for the timer that you specify [A or B or C or D]. Note that variables that are modified in interrupt handlers must be declared as volatile. For more information about the volatile keyword check out this tutorial.
 
 //Set the specified led to blink at the specified interval_ms.
 void blink_set(uint8_t led, uint16_t interval_ms){
-	LEDArray[(binaryToPosition(led)].blinkInterval = interval_ms;
+	LEDArray[led].blinkInterval = interval_ms;
 }
 
 //Update the state of the LEDs when a timer interrupt has occurred. This is the lower half handler for the timer interrupt, and should only be called if there was an unhandled timer interrupt that has occurred.
@@ -26,12 +24,13 @@ void blink_ms_timer_update(){
 			lastTime = globalTime;
 			if (LEDArray[i].counter >= LEDArray[i].blinkInterval) {
 				LEDArray[i].counter = 0;
-				led_toggle(i);
+				led_toggle(0b1 << (i));
 			}
 		}
 	}
 }
 
+/*
 uint8_t binaryToPosition(uint8_t binary){
 	for(uint8_t pos = 0; pos<8; pos++){
 		binary >> 1;
@@ -40,3 +39,4 @@ uint8_t binaryToPosition(uint8_t binary){
 
 	return 0;
 }
+*/
