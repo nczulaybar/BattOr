094d436f081a38147ebfa626803047e7fe5978c1
Sasha Maldonado <engineerajm@gmail.com>
2015-10-15 21:52:43 -0700

    Added converter helper method, incorporated into blink_set

--- blink.h
+++ blink.h
@@ -1,5 +1,5 @@
 void blink_init();
-	
+
 //Initialize the state of all of the LEDs in local state of the blink library to have their blinks disabled. Also, setup a timer that will fire an interrupt once every millisecond. Note that the timer is clocked off of the peripheral clock, and the peripheral clock is clocked off of the system clock.
 
 //ISR(TC[A,B,C,D]0_OVF_vect);
@@ -11,3 +11,6 @@ void blink_set(uint8_t led, uint16_t interval_ms);
 
 void blink_ms_timer_update();
 //Update the state of the LEDs when a timer interrupt has occurred. This is the lower half handler for the timer interrupt, and should only be called if there was an unhandled timer interrupt that has occurred.
+
+uint8_t binaryToPosition(uint8_t binary);
+//Convert a bit position variable (eg. 0b10, 0b100) into a uint8_t reflecting its position (2, 3 for the examples given)
