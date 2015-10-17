968dc6d2ae33c4ed29611f0239d398007e4b7c47
Sasha Maldonado <engineerajm@gmail.com>
2015-10-16 17:30:25 -0700

    Reduced blink frequency

--- BattOrProject1/BattOrProject1/main.c
+++ BattOrProject1/BattOrProject1/main.c
@@ -3,7 +3,7 @@
  *
  * Created: 10/8/2015 8:14:33 PM
  * Author : nczulaybar
- */ 
+ */
 
 #include <avr/io.h>
 #include <avr/interrupt.h> //Gives sei function that enables all interrupts
@@ -25,20 +25,19 @@ int main(void)
 {
 	sei();
 	PMIC.CTRL = PMIC.CTRL | 0b111;
-	
+
 	led_init();
 	blink_init();
-	blink_set(LED_GREEN_bm, 100/2);
-	blink_set(LED_YELLOW_bm, 250/2);
-	blink_set(LED_RED_bm, 500/2);
+	blink_set(LED_GREEN_bm, 100);
+	blink_set(LED_YELLOW_bm, 250);
+	blink_set(LED_RED_bm, 500);
 
-    while (1) 
+    while (1)
     {
 		if(checkBlink == 1) {
 			blink_ms_timer_update();
 			checkBlink = 0;
 		}
-		
+
     }
 }
-
